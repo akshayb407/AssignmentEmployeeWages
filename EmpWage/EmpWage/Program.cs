f0217995dc0411fce1c738057319b6fcfef82bf4
akshayb407 <akshayb407@gmail.com>
2022-06-20 17:22:51 +0530

    UC2 added

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -4,15 +4,47 @@ namespace EmpWage
 {
     internal class Program
     {
+        public const int IsPartTime = 2;
+        public const int IsFullTime = 1;
+        public const int IsAbsent = 0;
         static void Main(string[] args)
         {
-            {
-              GetemployeeAttandance program = new GetemployeeAttandance();
-                program.GetEmployeeAttandance();
+            Program program = new Program();
+            program.GetEmployeeWage();
+        }
 
+        public void GetEmployeeWage()
+        {
+
+            int WagePerHr = 20;
+
+            Random random = new Random();
+            int number = random.Next(0, 3);
+            Program program = new Program();
+            int empHrs = program.GetEmpHrs(number);
+
+            int Wage = WagePerHr * empHrs;
+
+            Console.WriteLine("Total wage is " + Wage);
+        }
+
+        public int GetEmpHrs(int number)
+        {
+            int empHrs = 0;
+
+            if (number == IsFullTime)
+            {
+                Console.WriteLine(number);
+                empHrs = 8;
             }
 
-           
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine(number);
+            }
+            return empHrs;
+
         }
     }
 }
