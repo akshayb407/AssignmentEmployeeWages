2c7a57aad635b3538eeb72c3e218cdc92a6da399
akshayb407 <akshayb407@gmail.com>
2022-06-20 17:29:42 +0530

    changes added

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -6,13 +6,25 @@ namespace EmpWage
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.GetemployeeAttendance();
+
+        }
+
+        public void GetemployeeAttendance()
+        {
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            if (value == 0)
             {
-              GetemployeeAttandance program = new GetemployeeAttandance();
-                program.GetEmployeeAttandance();
-
+                Console.WriteLine("Employee is absent");
+                Console.WriteLine(value);
             }
-
-           
+            else
+            {
+                Console.WriteLine("Employee is present");
+                Console.WriteLine(value);
+            }
         }
     }
 }
