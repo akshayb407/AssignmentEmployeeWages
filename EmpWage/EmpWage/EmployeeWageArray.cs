cc91937bf6eb9803994e8d083bcfae7b35890e0b
akshayb407 <akshayb407@gmail.com>
2022-06-22 20:52:29 +0530

    UC10 added

---
+++ EmpWage/EmpWage/EmployeeWageArray.cs
@@ -0,0 +1,79 @@
+﻿using EmpWage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class EmployeeWageArray
+    {
+        public const int IsPartTime = 2;
+        public const int IsFullTime = 1;
+        public const int IsAbsent = 0;
+
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWageArray;
+
+        public EmployeeWageArray()
+        {
+            this.companyEmpWageArray = new CompanyEmpWage[5];
+
+        }
+
+        public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numOfCompany++;
+
+        }
+
+        public void computeEmpWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].SetTotalEmpWage(this.computeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].ToString());
+            }
+        }
+
+        public  int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            //variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+
+            while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth
+                   && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IsFullTime:
+                        empHrs = 8;
+
+                        break;
+                    case IsPartTime:
+                        empHrs = 4;
+                        break;
+                    case IsAbsent:
+                        empHrs = 0;
+                        break;
+
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+         
+    }
+
+
+
+}
+
+
--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -6,12 +6,10 @@ namespace EmpWage
     {
         static void Main(string[] args)
         {
-            EmployeeWage dMart = new EmployeeWage("dMart", 20, 2, 10);
-            EmployeeWage Tata = new EmployeeWage("Tata", 30, 4, 20);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.tostring());
-            Tata.computeEmpWage();
-            Console.WriteLine(Tata.tostring());
+            EmployeeWageArray employeeWage = new EmployeeWageArray();
+            employeeWage.addCompanyEmpWage("DMart", 20, 2, 10);
+            employeeWage.addCompanyEmpWage("Tata", 30, 4, 20);
+            employeeWage.computeEmpWage();
 
         }
     }
